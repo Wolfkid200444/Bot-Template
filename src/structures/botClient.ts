94d2289f059f868dcce8ae43db6da4d086e59de0
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2023-05-23 10:41:24 -0400

    Remove Prefix in favor of Slash command

--- src/structures/botClient.ts
+++ src/structures/botClient.ts
@@ -3,8 +3,6 @@ import util from './utils.js';
 
 export default class BotClient extends Client {
 	util: util;
-	prefix: string;
-	token: string;
 	events: Collection<string, any>;
 	commands: Collection<string, any>;
 	interactions: Collection<string, any>;
@@ -29,10 +27,6 @@ export default class BotClient extends Client {
 	validate(options: Configuration) {
 		if (!options.TOKEN) throw new Error('You must pass the token for the client to work.');
 		this.token = options.TOKEN;
-
-		if (!options.PREFIX) throw new Error('You must pass the prefix for the client to work.');
-		if (typeof options.PREFIX !== 'string') throw new TypeError('Prefix must be a string.');
-		this.prefix = options.PREFIX;
 	}
 
 	async start() {
